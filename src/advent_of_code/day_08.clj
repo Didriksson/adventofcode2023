3fee9eca9825ff28832191208f53f331219a3d2c
Mattias Didriksson <mattias.didriksson@gmail.com>
2023-12-10 20:08:40 +0100

    Fortsatt ursch.

--- src/advent_of_code/day_08.clj
+++ src/advent_of_code/day_08.clj
@@ -1,35 +1,64 @@
-(ns advent-of-code.day-08 
+(ns advent-of-code.day-08
   (:require [clojure.string :as str]))
 
 
 (defrecord Step [node lv rv])
-(defrecord State [current steps])
-(def state (atom (->State nil nil)))
+(defrecord State [instructions current steps visited])
 
 (defn parse-step [input]
   (let [[node lv rv] (drop 1 (re-find #"(.+)\s=\s[(](.+),\s(.+)[)]" input))]
     (->Step node lv rv)))
 
-(defn find-next-node [state instruction]
-  (let [current-node (first (filter #(= (:current state) (:node %)) (:steps state)))]
+(defn find-next-node [instruction current steps]
+  (let [current-node (first (filter #(= current (:node %)) steps))]
     (if (= instruction \L)
-      (->State (:lv current-node) (:steps state))
-      (->State (:rv current-node) (:steps state)))))
+      (:lv current-node)
+      (:rv current-node))))
+
+(defn find-zzz [state numberofsteps]
+  (if (= (first (:current state)) "ZZZ")
+    numberofsteps
+    (let [instructions (:instructions state)
+          finstruction (first instructions)
+          restinstruciton (clojure.string/join (rest instructions))
+          cyclesinstructions (str restinstruciton finstruction)
+          next-node (find-next-node finstruction (first (:current state)) (:steps state))]
+      (recur (->State cyclesinstructions [next-node] (:steps state) []) (inc numberofsteps)))))
+
+
+(defn isAtZ [node]
+  (= (last node) \Z))
+
+
+(defn find-start-nodes [steps]
+  (filter #(= (last %) \A) (map :node steps)))
+
+(defn find-all-z [state numberofsteps]  
+  (if (every? isAtZ (:current state))
+    numberofsteps
+    (let [instructions (:instructions state)
+          finstruction (first instructions)
+          restinstruciton (clojure.string/join (rest instructions))
+          cyclesinstructions (str restinstruciton finstruction)
+          next-nodes (map #(find-next-node finstruction % (:steps state)) (:current state))]
+      (if (some #{next-nodes} (:visited state))
+        (println next-nodes " BOOM BOOM " (:visited state))
+        (recur (->State cyclesinstructions next-nodes (:steps state) (cons next-nodes (:visited state))) (inc numberofsteps))))))
 
 (defn part-1
   "Day 08 Part 1"
   [input]
   (let [[instructions elements] (str/split input #"\r\n\r\n")
-        steps (map parse-step (str/split elements #"\r\n"))]
-    (reset! state (->State "AAA" steps))
-    (->>
-     (cycle instructions)
-     (map find-next-node @state)
-     (reset! state)
-     (take-while #(not= (:current %) "ZZZ"))
-     (take 2))))
+        steps (map parse-step (str/split elements #"\r\n"))
+        state (->State instructions ["AAA"] steps [])]
+    (find-zzz state 0)))
+
+
 
 (defn part-2
   "Day 08 Part 2"
   [input]
-  input)
+  (let [[instructions elements] (str/split input #"\r\n\r\n")
+        steps (map parse-step (str/split elements #"\r\n"))
+        state (->State instructions (find-start-nodes steps) steps [(find-start-nodes steps)])]
+    (find-all-z state 0)))
--- src/advent_of_code/day_09.clj
+++ src/advent_of_code/day_09.clj
@@ -1,11 +1,33 @@
-(ns advent-of-code.day-09)
+(ns advent-of-code.day-09
+  (:require [clojure.string :as str]))
+
+(defn parse-line [line]
+  (map read-string (re-seq #"-?\d+" line)))
+
+(defn next-in-range [readings]
+  (let [differences (map #(reduce - (reverse %)) (partition 2 1 readings))]
+    (if (every? zero? differences)
+      (+ (last readings) 0)
+      (+ (last readings) (next-in-range differences)))))
+
+(defn previous-in-range [readings]
+  (let [differences (map #(reduce - (reverse %)) (partition 2 1 readings))]
+    (if (every? zero? differences)
+      (- (first readings) 0)
+      (- (first readings) (previous-in-range differences)))))
 
 (defn part-1
   "Day 09 Part 1"
   [input]
-  input)
+  (->>
+   (map parse-line (str/split-lines input))
+   (map next-in-range)
+   (reduce +)))
 
 (defn part-2
   "Day 09 Part 2"
   [input]
-  input)
+  (->>
+   (map parse-line (str/split-lines input))
+   (map previous-in-range)
+   (reduce +)))
