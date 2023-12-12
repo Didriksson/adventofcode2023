23fbc6d0e124b2f12410dcda40e19d9455d35ce6
Mattias Didriksson <mattias.didriksson@gmail.com>
2023-12-12 07:23:52 +0100

    Usch.

--- src/advent_of_code/day_10.clj
+++ src/advent_of_code/day_10.clj
@@ -2,6 +2,9 @@
   (:require [clojure.set :refer [union]]
             [clojure.string :as str]))
 
+
+(defrecord Path [point visited])
+
 (defn get-for-position [x y sketch]
   (nth (nth sketch y) x))
 
@@ -15,42 +18,66 @@
   (let [dp [(- to-x from-x) (- to-y from-y)]]
     (case dp
       [1 0] (cond
-              (some? (some #{val} "-7S")) true
+              (some? (some #{val} "-7SJ")) true
               :else false)
       [-1 0] (cond
-               (some? (some #{val} "-LS")) true
+               (some? (some #{val} "-LSF")) true
                :else false)
       [0 -1] (cond
-               (some? (some #{val} "7|S")) true
+               (some? (some #{val} "7|SF")) true
                :else false)
       [0 1] (cond
               (some? (some #{val} "J|LS")) true
-              :else false)
-      [] "Tomtom")))
+              :else false))))
 
 (defn potential-paths [val]
   (case val
-    \J [[0 -1] [-1 0]]
-    \7 [[0  1] [-1 0]]
-    \- [[-1  0] [1 0]]
     \| [[0  1] [0 -1]]
-    \L [[0  1] [1 0]]
+    \- [[-1  0] [1 0]]
+    \L [[0  -1] [1 0]]
+    \J [[0  -1] [-1 0]]
+    \7 [[0   1] [-1 0]]
+    \F [[0   1] [1 0]]
+    \. []
     \S [[1 0] [-1 0] [0 -1] [0 1]]))
 
 (defn eval-adjacents [x y sketch]
-  (let [potential (potential-paths (get-for-position x y sketch))]
+  (let [max-y (count sketch)
+        max-x (count (first sketch))
+        potential (filter #(and (<= 0 (+ x (first %)) (- max-x 1)) (<= 0 (+ y (second %)) (- max-y 1))) (potential-paths (get-for-position x y sketch)))]
     (filter #(accepts-from x y (+ x (first %)) (+ y (second %)) (get-for-position (+ x (first %)) (+ y (second %)) sketch)) potential)))
 
 
 (defn create-position [x y dx dy]
   [(+ x dx) (+ y dy)])
 
+(defn perform-eval-for-point [point visited sketch]
+  (->>
+   (eval-adjacents (first point) (second point) sketch)
+   (mapv #(create-position (first point) (second point) (first %) (second %)))
+   (filter #(nil? (some #{%} visited)))))
+
+(defn find-paths [path sketch]
+  (let
+   [point (:point path)
+    visited (:visited path)
+    new-points (perform-eval-for-point point visited sketch)]
+    (if (empty? new-points)
+      path
+      (mapv #(find-paths (->Path % (conj visited %)) sketch) new-points))))
+
 (defn part-1
   "Day 10 Part 1"
   [input]
   (let [sketch (str/split-lines input)
-        start (get-start-position sketch)]
-    (map #(create-position (:x start) (:y start) (first %) (second %)) (eval-adjacents (:x start) (:y start) sketch))))
+        start (get-start-position sketch)
+        evalutae-paths (find-paths (->Path [(:x start) (:y start)] (cons [(:x start) (:y start)] []))  sketch)]
+    (->>
+     (flatten evalutae-paths)
+     (map :visited)
+     (map count)
+     (apply max)
+     (#(/ % 2)))))
 
   (defn part-2
     "Day 10 Part 2"
